1a733274110e10a002e13751aa7d079e76c2a63f
ZackBridges <zcbridges@gmail.com>
2015-03-10 17:39:20 -0400

    Testing formspree

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Responsive behavior of video background HTML overlay
 var hello = function() {
    $("#hello").offset({
             top: .447 * $("#bgvideo").height(),
@@ -7,3 +8,11 @@ var hello = function() {
 
 $(window).resize(hello);
 $(document).ready(hello);
+
+//Conceals email address in HTML for contact form
+/*$(document).ready(function() {
+    var base64_email = 'emNicmlkZ2VzQGdtYWlsLmNvbQ==';
+    var base_url = '//forms.brace.io/';
+    var action = base_url + atob(base64_email);
+    $('#contact-form').attr('action', action);
+}); */
