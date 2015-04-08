8b901bf72b42b9a42d8c16f73c6e2622b1401e37
ZackBridges <zcbridges@gmail.com>
2015-04-08 17:58:09 -0400

    Conceal email address on contact form

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ $(window).resize(hello);
 $(document).ready(hello);
 
 //Conceals email address in HTML for contact form
-/*$(document).ready(function() {
+$(document).ready(function() {
     var base64_email = 'emNicmlkZ2VzQGdtYWlsLmNvbQ==';
-    var base_url = '//forms.brace.io/';
+    var base_url = 'http://formspree.io/';
     var action = base_url + atob(base64_email);
     $('#contact-form').attr('action', action);
-}); */
+});
 
 $(".htmlCSS").animate({width:"95%"}, 2100);
 $(".jsjqaj").animate({width:"70%"}, 2100);
@@ -23,3 +23,9 @@ $(".bootstrap").animate({width:"65%"}, 2100);
 $(".rwd").animate({width:"50%"}, 2100);
 $(".phpMSQL").animate({width:"20%"}, 2100);
 $(".angular").animate({width:"14%"}, 2100);
+
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
